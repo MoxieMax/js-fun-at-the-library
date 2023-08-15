8fca23541a62eaeb1e2bd903490d8593fcfe5c45
Max MacGillivray <mary.macgillivray@gmail.com>
2023-08-15 15:52:58 -0700

    Wrote a function that creates a character with a name, age, and pronouns. 5 tests passing.

--- src/book.js
+++ src/book.js
@@ -2,8 +2,16 @@ function createTitle(title) {
   return "The " + title;
 }
 
-function buildMainCharacter() {
-  
+function buildMainCharacter(name, age, pronouns) {
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  // var dictionary = {
+  //   name: "Webster's",
+  //   hasThesaurus: false
+  // };
 }
 
 module.exports = {
