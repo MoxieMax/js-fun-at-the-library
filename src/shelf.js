f21b4ca4135cb85bbb49161a8f7702a28ee665f9
Max MacGillivray <mary.macgillivray@gmail.com>
2023-08-15 18:26:48 -0700

    Created the shelfBook function that adds books to a shelf with a limit on shelf size.

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,13 @@
+function shelfBook(book, shelfArray) {
+  if (shelfArray.length < 3) {
+    shelfArray.unshift(book);
+  }
+  // , maxLength = Infinity
+  // shelfArray.unshift(book);
+}
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
